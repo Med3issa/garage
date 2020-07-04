913392f43b397750ac5d94798125066c08ad7cd9
Med3issa <59961025+Med3issa@users.noreply.github.com>
2020-07-04 10:08:15 +0100

    some changes

--- index.js
+++ index.js
@@ -5,42 +5,42 @@ const cors = require("cors");
 const mongoose = require("./configdb/db");
 
 const carController = require("./controllers/CarController");
-const userController=require("./controllers/UserController");
-const taxeController=require("./controllers/doc/TaxeController")
-const assuranceController=require('./controllers/doc/AssuranceController');
-const visiteController=require('./controllers/doc/VisiteController');
-const leasingController=require('./controllers/doc/leasingController');
-const vidangeController= require('./controllers/preventif/VidangeController');
-const filtreController=require('./controllers/preventif/FiltreController');
-const boujieController=require('./controllers/preventif/boujieController');
-const chaineController=require('./controllers/preventif/chaineController');
-const PneuController=require('./controllers/preventif/pneuController');
-const entretienController=require('./controllers/curatif/entretienController');
-
+const userController = require("./controllers/UserController");
+const taxeController = require("./controllers/doc/TaxeController");
+const assuranceController = require("./controllers/doc/AssuranceController");
+const visiteController = require("./controllers/doc/VisiteController");
+const leasingController = require("./controllers/doc/leasingController");
+const vidangeController = require("./controllers/preventif/VidangeController");
+const filtreController = require("./controllers/preventif/FiltreController");
+const boujieController = require("./controllers/preventif/boujieController");
+const chaineController = require("./controllers/preventif/chaineController");
+const PneuController = require("./controllers/preventif/pneuController");
+const entretienController = require("./controllers/curatif/entretienController");
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use("/car", carController);
-app.use("/user",userController);
-app.use("/ass",assuranceController);
-app.use("/taxe",taxeController);
-app.use("/visite",visiteController);
-app.use("/leasing",leasingController);
-app.use("/vid",vidangeController);
-app.use("/filtre",filtreController);
-app.use("/boujie",boujieController);
-app.use("/chaine",chaineController);
-app.use("/pneu",PneuController);
-app.use("/entretien",entretienController);
+app.use("/user", userController);
+app.use("/ass", assuranceController);
+app.use("/taxe", taxeController);
+app.use("/visite", visiteController);
+app.use("/leasing", leasingController);
+app.use("/vid", vidangeController);
+app.use("/filtre", filtreController);
+app.use("/boujie", boujieController);
+app.use("/chaine", chaineController);
+app.use("/pneu", PneuController);
+app.use("/entretien", entretienController);
 
-app.get("/", function (req, res) {
-  res.send("Welcome to the server !");
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to the server !");
 });
 
-const port = 3000 || process.env.PORT
+const port = 3000 || process.env.PORT;
 
-app.listen(port,()=>{
-    console.log("server started !");
-})
+app.listen(port, () => {
+  console.log("server started !");
+});
